66dde5df06f30676453f03ebe824d69d827921b5
Michael <mcla0203@umn.edu>
2009-08-21 20:19:56 -0500

    Adding src_test and first unit test

--- src/com/cn/server/Server.java
+++ src/com/cn/server/Server.java
@@ -1,5 +1,76 @@
 package com.cn.server;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class Server {
+	private int port;
+	private String hostName;
+	private ServerSocket socket;
+	
+	public Server() {
+		port = 7870;
+		hostName = "localhost";
+	}
+	
+	/**
+	 * Sets the hostname to the input string.
+	 * @param hostName
+	 */
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	
+	/**
+	 * Returns the hostName.
+	 * @return hostName
+	 */
+	public String getHostName() {
+		return hostName;
+	}
+	
+	/**
+	 * Sets the hostname to the input string.
+	 * @param hostName
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	/**
+	 * Returns the hostName.
+	 * @return hostName
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	public void setSocket(ServerSocket socket) {
+		this.socket = socket;
+	}
+
+	public ServerSocket getSocket() {
+		return socket;
+	}
+	
+	/**
+	 * Opens the socket that the server uses
+	 * @throws IOException
+	 */
+	public Boolean openSocket() {
+		try {
+			setSocket(new ServerSocket(port));
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	/**
+	 * Method to kick off the server.
+	 */
+	public Boolean start() {
+		return (this.openSocket() ? true : false);
+	}
 }
---
+++ src_test/com/cn/server/ServerTest.java
@@ -0,0 +1,18 @@
+package com.cn.server;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import com.cn.server.Server;
+
+/**
+ * Class to test to ServerStartup class.
+ * @author Michael
+ */
+public class ServerTest extends TestCase {
+
+	public void testMain() {
+		Server s = new Server();
+		Assert.assertTrue(s.start());
+	}
+	
+}
