51e54dbf146928a104a5f70ebd442ba47bb54832
Michael <mcla0203@umn.edu>
2009-08-24 07:51:19 -0500

    Remove some warnings

--- src/com/cn/client/Client.java
+++ src/com/cn/client/Client.java
@@ -8,9 +8,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.logging.Logger;
 
 import com.cn.constants.ClientConstants;
